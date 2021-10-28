e9e317cb860960285019c4c4206343202c38c2de
paulbricman <paulbricman@protonmail.com>
2021-10-28 17:14:18 +0200

    feat: add rss opml connector

--- connectors.py
+++ connectors.py
@@ -1,20 +1,35 @@
+import dateutil
 from newspaper import Article
 import feedparser
-from datetime import datetime
+from datetime import date, datetime
 from dateutil import parser
 from bs4 import BeautifulSoup
+import opml
 
 
-def fetch_from_online_article(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article.title, [article.text], 'online article'
+def fetch_from_opml(filename, max_days_ago):
+    aggregate_entries = []
 
+    for feed_entry in opml.parse(filename)[0]:
+        feed = feedparser.parse(feed_entry.xmlUrl)
+        entries = [e for e in feed['entries'] if (datetime.now() - parser.parse(e['published']).replace(tzinfo=None)).days < max_days_ago]
+        
+        for e_idx, e in enumerate(entries):
+            entries[e_idx]['author'] = feed['feed']['title'] 
+        
+        aggregate_entries += entries
 
-def fetch_from_rss_feed(url, days):
-    feed = feedparser.parse(url)
-    entries = [e for e in feed['entries'] if (datetime.now() - parser.parse(e['published']).replace(tzinfo=None)).days < days]
-    contents = [e['content'][0]['value'] for e in entries]
-    contents = [BeautifulSoup(e, 'html.parser').get_text() for e in contents]
-    return feed['feed']['title'], contents, 'RSS feed'
+    aggregate_entries = sorted(aggregate_entries, key=lambda x: parser.parse(x['published']))
+    aggregate_contents = [''] * len(aggregate_entries)
+    aggregate_titles = [''] * len(aggregate_entries)
+
+    for e_idx, e in enumerate(aggregate_entries):
+        if e.get('content') is not None:
+            aggregate_contents[e_idx] = e.get('content')[0]['value']
+        elif e.get('summary') is not None:
+            aggregate_contents[e_idx] = e.get('summary')
+        aggregate_titles[e_idx] = e.get('author') + ' | ' + e.get('title')
+
+    aggregate_contents = [BeautifulSoup(e, 'html.parser').get_text() for e in aggregate_contents]
+    
+    return aggregate_titles, aggregate_contents, 'RSS feed'
