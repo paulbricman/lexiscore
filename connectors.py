dd80452dcaa45212070fe30331a440daf5f6a197
paulbricman <paulbricman@protonmail.com>
2021-10-29 09:01:43 +0200

    feat: add bookmark file connector

--- connectors.py
+++ connectors.py
@@ -1,10 +1,12 @@
-import dateutil
 from newspaper import Article
 import feedparser
-from datetime import date, datetime
+from datetime import datetime
 from dateutil import parser
 from bs4 import BeautifulSoup
 import opml
+import re
+import time
+from urllib.parse import urlparse
 
 
 def fetch_from_opml(filename, max_days_ago):
@@ -32,4 +34,29 @@ def fetch_from_opml(filename, max_days_ago):
 
     aggregate_contents = [BeautifulSoup(e, 'html.parser').get_text() for e in aggregate_contents]
     
-    return aggregate_titles, aggregate_contents, 'RSS feed'
+    return aggregate_titles, aggregate_contents
+
+
+def fetch_from_bookmarks(filename, bookmark_folder, max_days_ago):
+    raw_bookmark_file = open(filename).read()
+    bookmark_soup = BeautifulSoup(raw_bookmark_file, 'html.parser')
+    folder_element = [e for e in bookmark_soup.find_all('h3') if re.compile(bookmark_folder).match(e.text)]
+    bookmarks = folder_element[0].parent()[1].find_all('a')
+    bookmarks = [e for e in bookmarks if (time.time() - float(e['add_date']) < max_days_ago * 60 * 60 * 24)]
+
+    bookmark_contents = []
+    bookmark_titles = []
+
+    for bookmark in bookmarks:
+        domain = urlparse(bookmark['href']).netloc.replace('www.', '')
+        article = Article(bookmark['href'])
+        article.build()
+        bookmark_contents += [article.text]
+        bookmark_titles += [domain + ' | ' + article.title]
+
+    return bookmark_titles, bookmark_contents
+
+'''
+import connectors
+x=connectors.fetch_from_bookmarks('data/bookmarks.html', 'Blogs', 200)
+'''
