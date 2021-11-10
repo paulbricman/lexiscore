f3081338d34f504047280ad582ef7dc596d51641
paulbricman <paulbricman@protonmail.com>
2021-11-10 07:33:23 +0100

    feat: get perplexity in a specific test

--- test_processing.py
+++ test_processing.py
@@ -2,6 +2,10 @@ import util
 import processing
 import connectors
 
+import torch
+from transformers import AutoTokenizer, AutoModelWithLMHead
+from tqdm import tqdm
+
 
 def test_skill():
     model = processing.init_model()
@@ -19,4 +23,39 @@ def test_skill():
     skill = processing.get_skill(results)
     print(skill, results)
 
-test_skill()
+
+def test_perplexity():
+    tokenizer = AutoTokenizer.from_pretrained('distilgpt2')
+    model = AutoModelWithLMHead.from_pretrained('distilgpt2')
+
+    content = connectors.fetch_from_opml('data/subscriptions.xml', 14)
+    content = list(content.values())[0]
+    print(content)
+    encodings = tokenizer(content, return_tensors='pt')
+    print(encodings['input_ids'].size(0))
+    
+    max_length = model.config.n_positions
+    stride = max_length
+
+    nlls = []
+    for i in tqdm(range(0, encodings.input_ids.size(1), stride)):
+        begin_loc = max(i + stride - max_length, 0)
+        end_loc = min(i + stride, encodings.input_ids.size(1))
+        trg_len = end_loc - i    # may be different from stride on last loop
+        input_ids = encodings.input_ids[:,begin_loc:end_loc]
+        target_ids = input_ids.clone()
+        target_ids[:,:-trg_len] = -100
+
+        with torch.no_grad():
+            outputs = model(input_ids, labels=target_ids)
+            neg_log_likelihood = outputs[0] * trg_len
+
+        nlls.append(neg_log_likelihood)
+
+    ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
+
+    print(ppl, nlls)
+
+
+
+test_perplexity()
